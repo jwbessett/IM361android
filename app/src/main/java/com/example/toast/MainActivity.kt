d0bfb16dfe2d4b1ea14e334a629b6eb3413f4fc0
jwbessett <60371078+jwbessett@users.noreply.github.com>
2020-04-08 16:48:31 -0500

    Interesting kotlin

--- app/src/main/java/com/example/toast/MainActivity.kt
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.toast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.System.console
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = "Jason rocks"
+
+        var i = 1
+
+        val button = findViewById<Button>(R.id.button);
+        button.setOnClickListener {
+            for (i in 1..100) {
+            if (i % 15 == 0) textView.text = "FizzBuzz"
+            else if (i % 3 == 0) textView.text = "Fizz"
+            else if (i % 5 == 0) textView.text = "Buzz"
+            else textView.text = i.toString()
+        }
+            i + 1
+        }
+
+
+
     }
 }
