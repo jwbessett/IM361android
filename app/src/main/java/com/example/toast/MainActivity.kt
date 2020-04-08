303d2aefbdf4298deb91e9e8ffec2b1c1088b592
jwbessett <60371078+jwbessett@users.noreply.github.com>
2020-04-08 17:33:32 -0500

    updated

--- app/src/main/java/com/example/toast/MainActivity.kt
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -11,27 +11,30 @@ import java.lang.System.console
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?): {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.text_view)
         textView.text = "Jason rocks"
 
-        var i = 1
+        var counter = 1
 
         val button = findViewById<Button>(R.id.button);
         button.setOnClickListener {
-            for (i in 1..100) {
+
+           loop@ for (i in 1..100) {
             if (i % 15 == 0) textView.text = "FizzBuzz"
             else if (i % 3 == 0) textView.text = "Fizz"
             else if (i % 5 == 0) textView.text = "Buzz"
             else textView.text = i.toString()
         }
-            i + 1
+
+            counter += 1
         }
 
-
+        val counterView = findViewById<TextView>(R.id.counter)
+        counterView.text = counter.toString(counter)
 
     }
 }
