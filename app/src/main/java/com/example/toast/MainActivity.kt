58845e0514b3822f864b233b3df001eda139deaa
jwbessett <60371078+jwbessett@users.noreply.github.com>
2020-04-13 11:48:53 -0500

    Added linear layout and tab layout

--- app/src/main/java/com/example/toast/MainActivity.kt
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -15,33 +15,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.text_view)
-        textView.text = "Jason rocks"
-
-        var counter = 0
-
-        val counterView = findViewById<TextView>(R.id.counter)
-
-        val button = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.floatingActionButton4);
-        button.setOnClickListener {
-
-           //loop@ for (i in 1..100) {
-            if (counter % 15 == 0) textView.text = "FizzBuzz"
-            else if (counter % 3 == 0) textView.text = "Fizz"
-            else if (counter % 5 == 0) textView.text = "Buzz"
-            else textView.text = counter.toString()
-       // }
-
-            counter += 1
-            counterView.text = "$counter"
-        }
-
-        val delete = findViewById<Button>(R.id.deleteButton)
-        delete.setOnClickListener {
-            counter = 0
-            counterView.text = "$counter"
-            textView.text = "Jason Rocks"
-        }
-
     }
 }
